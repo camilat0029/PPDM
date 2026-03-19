2dccea82690eab40ddaf4f8a865c63073acc94d1
Camila <camila.t.0029@gmail.com>
2026-03-19 11:03:26 -0300

    contador

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.meuapp;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +10,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     Button b;
+
+    TextView textView;
+
+    int contador=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +27,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         b=findViewById(R.id.button);
+        textView=findViewById((R.id.tv));
         b.setOnClickListener(v -> {
+
+            contador++;
+            textView.setText(Integer.toString(contador));
             b.setText("Blá");
+
         });
 
     }
