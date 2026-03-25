fd3b6965119bc1c5e64d748b349c38a58c4f0049
camila <camila.t.0029@gmail.com>
2026-03-25 12:09:45 -0300

    slide show com interface

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,62 +1,50 @@
 package com.example.meuapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.IntegerRes;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b;
+    Button bntAvancar, bntVoltar;
+    ImageView imageView;
 
-    TextView textView;
+    Integer imagens[] = new Integer[]{
+            R.drawable.cachorro,
+            R.drawable.gardem,
+            R.drawable.patinho,
+            R.drawable.happy,
+            R.drawable.porquinho
+    };
 
-    EditText edmin, edmax;
+    int posicao = 0;
 
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        b=findViewById(R.id.button);
-        textView=findViewById((R.id.tv));
-        edmin=findViewById(R.id.edMin);
-        edmax=findViewById(R.id.edMax);
+        bntVoltar=findViewById(R.id.buttonVoltar);
+        bntAvancar=findViewById(R.id.buttonAvancar);
+        imageView=findViewById(R.id.imageView);
 
-        b.setOnClickListener(v -> {
-
-            String minStr = edmin.getText().toString();
-            String maxStr = edmax.getText().toString();
-
-            if(minStr.isEmpty()){
-                edmin.setError("Informe um valor Minimo");
-                edmin.requestFocus();
-                return;
-            }
-            if(maxStr.isEmpty()){
-                edmax.setError("Informe um valor máximo");
-                edmax.requestFocus();
-                return;
-            }
-
-            int min = Integer.parseInt(minStr);
-            int max = Integer.parseInt(maxStr);
-
-            Random random = new Random();
-            int r=random.nextInt(min, max);
-            textView.setText(Integer.toString(r));
-
+        bntAvancar.setOnClickListener(v -> {
+            imageView.setImageResource(imagens[posicao]);
         });
+
+
 
     }
 }
