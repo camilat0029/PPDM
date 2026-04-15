f3a2a982f29aa59b32254a1165cd0c20faf01661
Camila <camila.t.0029@gmail.com>
2026-04-15 12:09:05 -0300

    Mudança de atividade pelo botão

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.meuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b;
+
+    EditText editTextMsg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        b=findViewById(R.id.button);
+        editTextMsg=findViewById(R.id.edtMensagem);
+
+        b.setOnClickListener(v -> {
+
+            Intent i = new Intent(getApplicationContext(),MainActivity2.class);
+            String mensagem=editTextMsg.getText().toString();
+            i.putExtra("msg", mensagem);
+            startActivity(i);
+
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/meuapp/MainActivity2.java
@@ -0,0 +1,44 @@
+package com.example.meuapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button botaoFinish;
+
+    TextView tv2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tv2=findViewById(R.id.textView2);
+
+        Intent i =getIntent();
+        Bundle b=i.getExtras();
+        String mensagem = b.getString("msg");
+        tv2.setText(mensagem);
+
+        botaoFinish=findViewById(R.id.btnFechar);
+        botaoFinish.setOnClickListener(v -> {
+            finish();
+        });
+
+    }
+}
