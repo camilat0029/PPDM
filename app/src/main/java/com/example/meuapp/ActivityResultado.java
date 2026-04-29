6008a46b630261efd31576614306778faaebf02f
Camila <camila.t.0029@gmail.com>
2026-04-29 10:05:24 -0300

    imc em duas telas iniciado

--- app/src/main/java/com/example/meuapp/ActivityResultado.java
+++ app/src/main/java/com/example/meuapp/ActivityResultado.java
@@ -3,7 +3,6 @@ package com.example.meuapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class activity_b extends AppCompatActivity {
+public class ActivityResultado extends AppCompatActivity {
 
-    TextView textView;
+    Button voltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_b);
+        setContentView(R.layout.activity_resultado);
 
-        Button bfechar = findViewById(R.id.button_finish);
-        textView=findViewById(R.id.tvMsg);
-
-        String txt= getIntent().getExtras().getString("msg");
-        textView.setText(txt);
-
-
-
-        bfechar.setOnClickListener(v -> {
-            finish();
-        });
+        Button voltar=findViewById(R.id.voltar);
+        voltar.setOnClickListener(v -> {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    finish();
+                }
+        );
     }
 }
--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,35 +1,116 @@
 package com.example.meuapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
+    Button bntCalcularImc;
+    ImageView imageView;
 
+    EditText peso, altura;
+
+    TextView imcFinal, imcClassificacao;
+
+    Double imc, pesoF, alturaF;
+
+    Integer imagens[] = new Integer[]{
+            R.drawable.perfil,
+            R.drawable.abaixopeso,
+            R.drawable.normal,
+            R.drawable.sobrepeso,
+            R.drawable.obesidade1,
+            R.drawable.obesidade2,
+            R.drawable.obesidade3,
+    };
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        editText=findViewById(R.id.editText);
 
-        Button button=findViewById(R.id.button);
-        button.setOnClickListener(v -> {
-            Intent intent= new Intent(getApplicationContext(), activity_b.class);
-            String msg=editText.getText().toString();
+        bntCalcularImc=findViewById(R.id.buttonCalcular);
+        peso=findViewById(R.id.Peso);
+        altura=findViewById(R.id.Altura);
+        imcFinal=findViewById(R.id.Imc);
+        imcClassificacao=findViewById(R.id.tvClassificacao);
+        imageView=findViewById(R.id.imageView);
 
-            intent.putExtra("msg", msg);
+        imageView.setImageResource(imagens[0]);
+
+        bntCalcularImc.setOnClickListener(v -> {
+
+            String alturaS = altura.getText().toString();
+            String pesoS = peso.getText().toString();
+
+            if(alturaS.isEmpty()){
+                altura.setError("Informe um valor Minimo");
+                altura.requestFocus();
+                return;
+            }
+
+            if (pesoS.isEmpty()){
+                peso.setError("Informe um valor Minimo");
+                peso.requestFocus();
+                return;
+            }
+
+            alturaF = Double.parseDouble(alturaS);
+            pesoF = Double.parseDouble(pesoS);
+
+            imc = (pesoF/(alturaF*alturaF));
+            DecimalFormat dc = new DecimalFormat("##.##");
+
+            if(imc < 18.5){
+                imageView.setImageResource(imagens[1]);
+                imcFinal.setText("IMC = " + dc.format(imc));
+                imcClassificacao.setText("Classificação = Abaixo do Peso");
+            } else if(imc >= 18.5 && imc < 24.9){
+                imageView.setImageResource(imagens[2]);
+                imcFinal.setText("IMC = " + dc.format(imc));
+                imcClassificacao.setText("Classificação = Normal");
+            } else if(imc >= 25 && imc < 29.9){
+                imageView.setImageResource(imagens[3]);
+                imcFinal.setText("IMC = " + dc.format(imc));
+                imcClassificacao.setText("Classificação = Sobrepeso");
+            } else if(imc >= 30 && imc < 34.9){
+                imageView.setImageResource(imagens[4]);
+                imcFinal.setText("IMC = " + dc.format(imc));
+                imcClassificacao.setText("Classificação = Obesidade grau 1");
+            } else if(imc >= 35 && imc < 39.9){
+                imageView.setImageResource(imagens[5]);
+                imcFinal.setText("IMC = " + dc.format(imc));
+                imcClassificacao.setText("Classificação = Obesidade grau 2");
+            } else if(imc >= 40){
+                imageView.setImageResource(imagens[6]);
+                imcFinal.setText("IMC = " + dc.format(imc));
+                imcClassificacao.setText("Classificação = Obesidade grau 3");
+            }
+
+            Intent intent = new Intent(getApplicationContext(), ActivityResultado.class);
+
+            alturaF = Double.parseDouble(alturaS);
+            pesoF = Double.parseDouble(pesoS);
+
+            intent.putExtra("Altura", alturaF);
+            intent.putExtra("Peso", pesoF);
+
             startActivity(intent);
-        });
 
+        });
     }
 }
