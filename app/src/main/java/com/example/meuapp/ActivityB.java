a0e92bda00c1c6b556ff93dfba4a0a9e53226dab
Camila <camila.t.0029@gmail.com>
2026-05-06 10:32:18 -0300

    listagem

---
+++ app/src/main/java/com/example/meuapp/ActivityB.java
@@ -0,0 +1,34 @@
+package com.example.meuapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.BreakIterator;
+
+public class ActivityB extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_b);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+
+        String nome =getIntent().getExtras().getString("nome");
+        TextView tv=findViewById(R.id.textView);
+        tv.setText(nome);
+
+    }
+}
--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.meuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,10 +14,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    String nomes[] = new String[]{"Camila", "Lara", "Júlia", "Letícia", "Gabriela", "Yasmin", "Sofia"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this, //contexto
+                android.R.layout.simple_list_item_1, // Recurso de layout xml
+                android.R.id.text1, //id do textView interno do Layout
+                nomes //array de String (dataSource)
+
+        );
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+
+            Intent i = new Intent(getApplicationContext(),ActivityB.class);
+            //ACRESCENTAR DADO NA INTENÇÃO
+            i.putExtra("nome", nomes[position]); // nome e valor
+
+            startActivity(i);
+
+
+        });
+
     }
 }
